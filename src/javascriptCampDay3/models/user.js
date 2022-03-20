65cbeb17ba7beded841848c7ebc3a9aee5b45d8c
Ozan Polat <ozanboranpolat@gmail.com>
2022-03-20 20:40:20 +0300

    Add files via upload

---
+++ src/javascriptCampDay3/models/user.js
@@ -0,0 +1,8 @@
+export default class User{
+    constructor(id,firstName,lastName,city){
+        this.firstName= firstName
+        this.id = id
+        this.lastName = lastName
+        this.city = city
+    }
+}
